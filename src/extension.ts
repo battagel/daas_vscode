6ad39b69e8b2eb537fa454ca7cfe117c39ecbf70
Matthew Battagel <battagel@Matthews-Work-MacBook-Pro.local>
2024-01-08 23:45:20 +0000

    Added status bar and help page

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,31 @@ import { API } from "./api/api";
 
 export function activate(context: vscode.ExtensionContext) {
 	const api_url = "http://127.0.0.1:8080";
+	const help_url = "https://matthewbattagel.co.uk/#/help/antonium";
 	const api = new API(api_url);
 
-    let disposable = vscode.languages.registerHoverProvider(['plaintext', 'c', 'typescript', 'javascript'], {
+	console.log("DaaS for VSCode successfully launched! 🚀");
+	// vscode.window.showInformationMessage('DaaS for VSCode acitve! 🚀');
+
+	// Status bar item
+    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+    statusBarItem.text = "DaaS";
+    statusBarItem.tooltip = "Open DaaS Help";
+    statusBarItem.command = "daas.help";
+    context.subscriptions.push(statusBarItem);
+	statusBarItem.show();
+
+	let helpCommand = vscode.commands.registerCommand('daas.help', function() {
+		// Open the URL in the default web browser
+		vscode.env.openExternal(vscode.Uri.parse(help_url));
+	});
+
+    let onHover = vscode.languages.registerHoverProvider('*', {
         async provideHover(document, position, token) {
             const wordRange = document.getWordRangeAtPosition(position);
             const word = wordRange ? document.getText(wordRange) : '';
 
-			console.log(`${word}`);
+			// console.log(`${word}`);
             if (word) {
 				const phrase_item = await api.get(word);
 				console.log(phrase_item);
@@ -70,7 +87,6 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(onHover);
+    context.subscriptions.push(helpCommand);
 }
-
-export function deactivate() {}
